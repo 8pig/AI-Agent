28348f85846400ca78444f0dad31ce3f9508a63e
zhou <zhou773@foxmail.com>
2025-11-04 23:19:16 +0800

    feat: FewShotPromptTemplate 提示词

--- app/bailian/banlian_tools.py
+++ app/bailian/banlian_tools.py
@@ -11,20 +11,20 @@ llm = ChatOpenAI(
     streaming=True,
 )
 
-system_message_template = ChatMessagePromptTemplate.from_template(
-    template =  "你是一个{role}专家, 帮助用户解答{domain}领域的问题, 回答内容请遵守法律法规",
-    role="system"
-)
-
-human_message_template = ChatMessagePromptTemplate.from_template(
-    template = "用户的问题: {question}",
-    role="user"
-)
-
-chat_prompt_template = ChatPromptTemplate.from_messages([
-    system_message_template,
-    human_message_template
-])
+# system_message_template = ChatMessagePromptTemplate.from_template(
+#     template =  "你是一个{role}专家, 帮助用户解答{domain}领域的问题, 回答内容请遵守法律法规",
+#     role="system"
+# )
+#
+# human_message_template = ChatMessagePromptTemplate.from_template(
+#     template = "用户的问题: {question}",
+#     role="user"
+# )
+#
+# chat_prompt_template = ChatPromptTemplate.from_messages([
+#     system_message_template,
+#     human_message_template
+# ])
 
 
 # 抽象到上面  system 与 user prompt temp
@@ -50,27 +50,15 @@ examples = [
     {"input": "pen 翻译为中文", "output": "钢笔"},
 ]
 
-
 few_shot_prompt_template = FewShotPromptTemplate(
     examples = examples,
     example_prompt=PromptTemplate.from_template(example_prompt),
-    #
     prefix="请将一下英文翻译中文",
     suffix="输入:{text}\n 输出:",
     input_variables=["text"], # text 替换suffix 中的 {text}
 )
 
-
-
-# # 创建提示词模板
-# prompt_template = PromptTemplate.from_template("今天{something}真不错")
-# #
-# # # 模板+变量 = 提示词
-# prompt = prompt_template.format(something="天气")
-#
-# print(prompt_template)
-
-prompt = few_shot_prompt_template.format(text="what fucking are you doing")
+prompt = few_shot_prompt_template.format(text="wtf")
 print(prompt)
 resp = llm.stream(prompt)
 
@@ -82,3 +70,13 @@ for chunk in resp:
 # for chunk in llm.stream(prompt):
 #     if hasattr(chunk, 'content'):
 #         print(chunk.content, end="", flush=True)
+
+
+
+# # 创建提示词模板
+# prompt_template = PromptTemplate.from_template("今天{something}真不错")
+# #
+# # # 模板+变量 = 提示词
+# prompt = prompt_template.format(something="天气")
+#
+# print(prompt_template)
